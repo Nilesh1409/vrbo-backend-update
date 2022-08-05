e22f7b678a7ed99fdb90f1d11c1a1f95dfc37a22
Nilesh1409 <nileshtiwari7054@gmail.com>
2022-08-05 15:12:48 +0530

    backend

---
+++ Database/index.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+require('dotenv').config()
+async function connectDatabase()
+{
+    const dbURI=process.env.MONGOURI;
+    try {
+        await mongoose.connect(dbURI);
+        console.log("Database connected ")
+    } catch (error) {
+        console.log("Something went wrong with database")
+    }
+}
+
+module.exports=connectDatabase;
