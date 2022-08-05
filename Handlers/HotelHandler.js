5e71e7d97f4c959164c6417387709e42c6d321a0
Nilesh1409 <nileshtiwari7054@gmail.com>
2022-08-05 15:48:42 +0530

    done

--- Handlers/HotelHandler.js
+++ Handlers/HotelHandler.js
@@ -1,11 +1,17 @@
 const Hotels=require("../Database/Hotels");
 
 
+// async function searchHotel(req,res,next)
+// {
+//     const {city}=req.params;
+//     const data=await Hotels.find({city:{$regex:`^${city}`,$options:"$i"}});
+//     res.status(200).send({data:data});
+// }
+
 async function searchHotel(req,res,next)
 {
     const {city}=req.params;
-    const data=await Hotels.find({city:{$regex:`^${city}`,$options:"$i"}});
+    const data=await Hotels.find({id : city});
     res.status(200).send({data:data});
 }
-
 module.exports=searchHotel;
